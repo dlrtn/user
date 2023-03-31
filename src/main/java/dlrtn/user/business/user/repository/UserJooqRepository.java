ea63760ece0192b613c66bb7de07fa19a235a564
dlrtn <87226112+dlrtn@users.noreply.github.com>
2023-04-01 00:34:08 +0900

    chore: 레포지토리 로직 이동 및 JooqRepository 생성

---
+++ src/main/java/dlrtn/user/business/user/repository/UserJooqRepository.java
@@ -0,0 +1,12 @@
+package dlrtn.user.business.user.repository;
+
+import dlrtn.user.business.user.model.domain.User;
+
+public class UserJooqRepository implements UserRepository {
+
+    @Override
+    public void saveUser(User user) {
+
+    }
+
+}
